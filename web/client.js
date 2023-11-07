092fa307da821ce7117060df975e1166ac6e19e2
m3rashid <m3rashid.hussain@gmail.com>
2023-11-07 10:05:43 +0530

    init: I am BATMAN

--- web/client.js
+++ web/client.js
@@ -1,52 +1,13 @@
-const { EchoRequest,
-	ServerStreamingEchoRequest } = require('./echo_pb.js');
-const { EchoServiceClient } = require('./echo_grpc_web_pb.js');
-const { EchoApp } = require('../echoapp.js');
-const grpc = {};
-grpc.web = require('grpc-web');
+const { EchoRequest, EchoResponse } = require('../node-server/proto/hello_pb.js');
+const { EchoServiceClient } = require('./proto/hello_grpc_web_pb.js');
 
-/** Sample interceptor implementation */
-const StreamResponseInterceptor = function () { };
+var echoService = new EchoServiceClient('http://localhost:8080');
 
-/**
- * @template REQUEST, RESPONSE
- * @param {!Request<REQUEST, RESPONSE>} request
- * @param {function(!Request<REQUEST,RESPONSE>):!ClientReadableStream<RESPONSE>}
- *     invoker
- * @return {!ClientReadableStream<RESPONSE>}
- */
-StreamResponseInterceptor.prototype.intercept = function (request, invoker) {
-	const InterceptedStream = function (stream) {
-		this.stream = stream;
-	};
-	InterceptedStream.prototype.on = function (eventType, callback) {
-		if (eventType == 'data') {
-			const newCallback = (response) => {
-				response.setMessage('[Intcpt Resp1]' + response.getMessage());
-				callback(response);
-			};
-			this.stream.on(eventType, newCallback);
-		} else {
-			this.stream.on(eventType, callback);
-		}
-		return this;
-	};
-	var reqMsg = request.getRequestMessage();
-	reqMsg.setMessage('[Intcpt Req1]' + reqMsg.getMessage());
-	return new InterceptedStream(invoker(request));
-};
+var request = new EchoRequest();
+request.setMessage('Hello World!');
 
-var opts = { 'streamInterceptors': [new StreamResponseInterceptor()] };
-var echoService = new EchoServiceClient('http://' + window.location.hostname + ':8080', null,
-	null);
-//                                      opts);
-
-var echoApp = new EchoApp(
-	echoService,
-	{
-		EchoRequest: EchoRequest,
-		ServerStreamingEchoRequest: ServerStreamingEchoRequest
-	}
-);
-
-echoApp.load();
+echoService.echo(request, {}, function (err, response) {
+	// ...
+	console.log("route hit")
+	console.log(response.getMessage());
+});
